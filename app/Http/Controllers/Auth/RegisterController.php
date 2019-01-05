2c83717bf2af103fce6f2f14d05c075426ae43ef
Fabricio <fabriciomelosilva@gmail.com>
2019-01-05 12:09:09 -0300

    Adição da role admin nos usuários criados no back

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -3,6 +3,9 @@
 namespace App\Http\Controllers\Auth;
 
 use App\User;
+use App\Role;
+
+
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Foundation\Auth\RegistersUsers;
@@ -62,10 +65,17 @@ class RegisterController extends Controller
      */
     protected function create(array $data)
     {
-        return User::create([
+        $user = User::create([
             'name' => $data['name'],
             'email' => $data['email'],
             'password' => bcrypt($data['password']),
         ]);
+
+        $findRole = Role::where('name','admin')->first();
+        
+        $user->roles()->attach($findRole);
+
+        return $user;
+
     }
 }
