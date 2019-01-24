201892c01ed5bac4b47010190efe215c741dfcd0
750370642 <gabriel.melo@geq.com.br>
2019-01-24 19:34:10 -0200

    Criado novo layout header

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,38 +1,26 @@
 <!doctype html>
-<html>
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <title>Diário Digital</title>
-    <link href="/css/bootstrap.min.css" rel="stylesheet">
-    <link href="/css/style.css" rel="stylesheet">
-    <link href="/simulor/admin/dist/bootstrap.min.css" rel="stylesheet">
-    <link href="/simulor/admin/dist/app.css" rel="stylesheet">
-    <link href="/simulor/admin/dist/icons.min.css" rel="stylesheet">
+<html lang="pt-BR">
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+		<title>Diário Digital</title>
+		<link href="/css/bootstrap.min.css" rel="stylesheet">
+		<link href="/css/style.css" rel="stylesheet">
+		<link href="/simulor/admin/dist/css/bootstrap.min.css" rel="stylesheet">
+		<link href="/simulor/admin/dist/css/app.css" rel="stylesheet">
+		<link href="/simulor/admin/dist/css/icons.min.css" rel="stylesheet">
+	</head>
 
-    <script src="/simulor/admin/dist/js/vendor.min.js"></script>
-    <script src="/simulor/admin/dist/js/app.min.js"></script>
+	<body>
+		@include('partials.header')
 
-  </head>
-  
-  <nav style="background-color: #26a69a;">
-        <div class="nav-wrapper">
-          <a href="#" class="brand-logo center">Logo</a>
-          <ul id="nav-mobile" class="left hide-on-med-and-down">
-            @if(Auth::guard('web')->check())
-                <li><a href="{{ route('edicaoGet') }}">Cadastrar Edição</a></li>
-                <li><a href="{{ route('lista_edicao') }}">Lista de Edições</a></li>
-                <li><a href="{{ route('register') }}">Usuários</a></li>
+		<main class="wrapper">
+			<div class="container-fluid">
+				@yield('content')
+			</div>
+		</main>
 
-            @endif    
-        </ul>
-        </div>
-      </nav>
-  
-<body>
-
-  <main class="site-main">
-    @yield('content')
-  </main>
-  </body>
+		<script src="/simulor/admin/dist/js/vendor.min.js"></script>
+		<script src="/simulor/admin/dist/js/app.min.js"></script>
+	</body>
 </html>
---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,212 @@
+<!-- Navigation Bar-->
+<header id="topnav">
+    <nav class="navbar-custom">
+
+        <div class="container-fluid">
+            <ul class="list-unstyled topbar-right-menu float-right mb-0">
+
+                <li class="dropdown notification-list">
+                    <!-- Mobile menu toggle-->
+                    <a class="navbar-toggle nav-link">
+                        <div class="lines">
+                            <span></span>
+                            <span></span>
+                            <span></span>
+                        </div>
+                    </a>
+                    <!-- End mobile menu toggle-->
+                </li>
+
+                <li class="dropdown notification-list">
+                    <a class="nav-link dropdown-toggle arrow-none" data-toggle="dropdown" href="#" role="button" aria-haspopup="false" aria-expanded="false">
+                        <i class="fe-bell noti-icon"></i>
+                        <span class="badge badge-danger noti-icon-badge">2</span>
+                    </a>
+                    <div class="dropdown-menu dropdown-menu-right dropdown-menu-animated dropdown-lg">
+
+                        <!-- item-->
+                        <div class="dropdown-item noti-title">
+                            <h5 class="m-0">
+                                <span class="float-right">
+                                    <a href="" class="text-dark">
+                                        <small>Clear All</small>
+                                    </a>
+                                </span>Notification</h5>
+                        </div>
+
+                        <div class="slimscroll noti-scroll">
+
+                            <!-- item-->
+                            <a href="javascript:void(0);" class="dropdown-item notify-item active">
+                                <div class="notify-icon">
+                                    <img src="assets/images/users/avatar-2.jpg" class="img-fluid rounded-circle" alt="" /> </div>
+                                <p class="notify-details">Cristina Pride</p>
+                                <p class="text-muted mb-0 user-msg">
+                                    <small>Hi, How are you? What about our next meeting</small>
+                                </p>
+                            </a>
+
+                            <!-- item-->
+                            <a href="javascript:void(0);" class="dropdown-item notify-item">
+                                <div class="notify-icon bg-light">
+                                    <i class="mdi mdi-comment-account-outline"></i>
+                                </div>
+                                <p class="notify-details">Caleb Flakelar commented on Admin
+                                    <small class="text-muted">1 min ago</small>
+                                </p>
+                            </a>
+
+                            <!-- item-->
+                            <a href="javascript:void(0);" class="dropdown-item notify-item">
+                                <div class="notify-icon">
+                                    <img src="assets/images/users/avatar-4.jpg" class="img-fluid rounded-circle" alt="" /> </div>
+                                <p class="notify-details">Karen Robinson</p>
+                                <p class="text-muted mb-0 user-msg">
+                                    <small>Wow ! this admin looks good and awesome design</small>
+                                </p>
+                            </a>
+
+                            <!-- item-->
+                            <a href="javascript:void(0);" class="dropdown-item notify-item">
+                                <div class="notify-icon bg-light">
+                                    <i class="mdi mdi-account-plus"></i>
+                                </div>
+                                <p class="notify-details">New user registered.
+                                    <small class="text-muted">5 hours ago</small>
+                                </p>
+                            </a>
+
+                            <!-- item-->
+                            <a href="javascript:void(0);" class="dropdown-item notify-item">
+                                <div class="notify-icon bg-light">
+                                    <i class="mdi mdi-comment-account-outline"></i>
+                                </div>
+                                <p class="notify-details">Caleb Flakelar commented on Admin
+                                    <small class="text-muted">4 days ago</small>
+                                </p>
+                            </a>
+
+                            <!-- item-->
+                            <a href="javascript:void(0);" class="dropdown-item notify-item">
+                                <div class="notify-icon bg-light">
+                                    <i class="mdi mdi-heart"></i>
+                                </div>
+                                <p class="notify-details">Carlos Crouch liked
+                                    <b>Admin</b>
+                                    <small class="text-muted">13 days ago</small>
+                                </p>
+                            </a>
+                        </div>
+
+                        <!-- All-->
+                        <a href="javascript:void(0);" class="dropdown-item text-center text-primary notify-item notify-all">
+                            View all
+                            <i class="fi-arrow-right"></i>
+                        </a>
+
+                    </div>
+                </li>
+
+                <li class="dropdown notification-list">
+                    <a class="nav-link dropdown-toggle nav-user mr-0" data-toggle="dropdown" href="#" role="button" aria-haspopup="false" aria-expanded="false">
+                        <img src="assets/images/users/avatar-1.jpg" alt="user-image" class="rounded-circle">
+                        <small class="pro-user-name ml-1">
+                            Morgan K
+                        </small>
+                    </a>
+                    <div class="dropdown-menu dropdown-menu-right dropdown-menu-animated profile-dropdown ">
+                        <!-- item-->
+                        <div class="dropdown-header noti-title">
+                            <h6 class="text-overflow m-0">Welcome !</h6>
+                        </div>
+
+                        <!-- item-->
+                        <a href="javascript:void(0);" class="dropdown-item notify-item">
+                            <i class="fe-user"></i>
+                            <span>My Account</span>
+                        </a>
+
+                        <!-- item-->
+                        <a href="javascript:void(0);" class="dropdown-item notify-item">
+                            <i class="fe-settings"></i>
+                            <span>Settings</span>
+                        </a>
+
+                        <!-- item-->
+                        <a href="javascript:void(0);" class="dropdown-item notify-item">
+                            <i class="fe-lock"></i>
+                            <span>Lock Screen</span>
+                        </a>
+
+                        <div class="dropdown-divider"></div>
+
+                        <!-- item-->
+                        <a href="javascript:void(0);" class="dropdown-item notify-item">
+                            <i class="fe-log-out"></i>
+                            <span>Logout</span>
+                        </a>
+
+                    </div>
+                </li>
+
+            </ul>
+
+            <ul class="list-inline menu-left mb-0">
+                <li class="float-left">
+                    <a href="index.html" class="logo">
+                        <span class="logo-lg">
+                            <img src="assets/images/logo.png" alt="" height="18">
+                        </span>
+                        <span class="logo-sm">
+                            <img src="assets/images/logo-sm.png" alt="" height="28">
+                        </span>
+                    </a>
+                </li>
+                <li class="app-search">
+                    <form>
+                        <input type="text" placeholder="Search..." class="form-control">
+                        <button type="submit" class="sr-only"></button>
+                    </form>
+                </li>
+            </ul>
+        </div>
+
+    </nav>
+    <!-- end topbar-main -->
+
+    <div class="topbar-menu">
+        <div class="container-fluid">
+            <div id="navigation">
+                <!-- Navigation Menu-->
+                <ul class="navigation-menu">
+
+					@if(Auth::guard('web')->check())
+						<li class="has-submenu">
+							<a href="{{ route('edicaoGet') }}">
+								<i class="fe-airplay"></i>Cadastrar Edição
+							</a>
+						</li>
+						<li class="has-submenu">
+							<a href="{{ route('lista_edicao') }}">
+								<i class="fe-airplay"></i>Lista de Edições
+							</a>
+						</li>
+						<li class="has-submenu">
+							<a href="{{ route('register') }}">
+								<i class="fe-airplay"></i>Usuários
+							</a>
+						</li>
+					@endif
+
+                </ul>
+                <!-- End navigation menu -->
+
+                <div class="clearfix"></div>
+            </div>
+            <!-- end #navigation -->
+        </div>
+        <!-- end container -->
+    </div>
+    <!-- end navbar-custom -->
+</header>
+<!-- End Navigation Bar-->
