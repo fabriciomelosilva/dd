78110d757850fb12409d105072c5e123b1c1c265
Fabricio <fabriciomelosilva@gmail.com>
2019-02-07 15:31:43 -0300

    Ajuste campo data

--- resources/views/admin/pages/edicao.blade.php
+++ resources/views/admin/pages/edicao.blade.php
@@ -51,7 +51,7 @@
 					<div class="form-group row">
 						<label class="col-sm-2 col-form-label">Data</label>
 						<div class="col-sm-10">
-							<input class="form-control" name="data_edicao" type="date" id="calendario">
+							<input class="form-control" name="data_edicao" type="text" id="calendario">
 						</div>
 					</div>
 
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -24,6 +24,9 @@
 		<script src="/simulor/admin/dist/js/app.min.js"></script>
 
 		<link rel="stylesheet" href="http://code.jquery.com/ui/1.9.0/themes/base/jquery-ui.css" />
+		<script src="http://code.jquery.com/jquery-1.8.2.js"></script>
+		<script src="http://code.jquery.com/ui/1.9.0/jquery-ui.js"></script>
+
 		<script src="/js/custom.js"></script>
 
 	</body>
