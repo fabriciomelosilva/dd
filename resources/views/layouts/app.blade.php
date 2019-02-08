7f0892b4d4aa8d624d990aaeec795ad7a814c4c5
Fabricio <fabriciomelosilva@gmail.com>
2019-02-08 08:40:38 -0300

    Ajustes scripts

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,17 +9,12 @@
 		<link href="/simulor/admin/dist/css/app.css" rel="stylesheet">
 		<link href="/simulor/admin/dist/css/icons.min.css" rel="stylesheet">
 		<link href="/css/style.css" rel="stylesheet">
-
-
 		<script src="/simulor/admin/dist/js/vendor.min.js"></script>
 		<script src="/simulor/admin/dist/js/app.min.js"></script>
-
 		<link rel="stylesheet" href="http://code.jquery.com/ui/1.9.0/themes/base/jquery-ui.css" />
-		<script src="http://code.jquery.com/jquery-1.8.2.js"></script>
-		<script src="http://code.jquery.com/ui/1.9.0/jquery-ui.js"></script>
-
+		<script src="/js/jquery-1.8.2.js"></script>
+		<script src="/js/jquery-ui.js"></script>
 		<script src="/js/custom.js"></script>
-
 
 	</head>
 
