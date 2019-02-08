1d329c96d68bdef651a1ae10656cead34543aabb
Fabricio <fabriciomelosilva@gmail.com>
2019-02-08 08:47:29 -0300

    Ajustes na tela de edição

--- resources/views/admin/pages/edicaoedit.blade.php
+++ resources/views/admin/pages/edicaoedit.blade.php
@@ -1,75 +1,91 @@
 @extends('layouts.app')
 @section('content')
 
+<!-- start error -->
+@if ($errors->any())
+<div class="alert alert-primary alert-dismissible bg-danger text-white border-0 fade show mt-2" role="alert">
+	<button type="button" class="close" data-dismiss="alert" aria-label="Close">
+		<span aria-hidden="true">×</span>
+	</button>
+	@foreach ($errors->all() as $error)
+		{{ $error }}
+	@endforeach
+
+</div>
+
+@endif
+
+@if (session()->has('flash.message'))
+	<div class="alert alert-primary alert-dismissible bg-sucess text-white border-0 fade show mt-2" role="alert">
+		{{ session('flash.message') }}
+	</div>
+@endif
+<!-- end error -->
+
 <!-- start page title -->
 <div class="row">
 	<div class="col-12">
 		<div class="page-title-box">
 			<div class="page-title-right">
 				<ol class="breadcrumb m-0">
-					<li class="breadcrumb-item"><a href="#">Edição</a></li>
-					<li class="breadcrumb-item active"><a href="#">Editar</a></li>
+					<li class="breadcrumb-item"><a href="#">Simulor</a></li>
+					<li class="breadcrumb-item"><a href="#">Layouts</a></li>
+					<li class="breadcrumb-item active">Light Topbar</li>
 				</ol>
 			</div>
-			<h4 class="page-title">Edições</h4>
+			<h4 class="page-title">Cadastro</h4>
 		</div>
 	</div>
 </div>
 
 
-<div class="container">
-    <form class="form" id="formulario" action="{{route('editarEdicaoPost', ['edicao'=>$edicao->id])}}" method="post" enctype="multipart/form-data">
+<!-- end page title -->
+<div class="row">
+	<div class="col-12">
+		<div class="card">
+			<div class="card-body">
+				<h4 class="header-title">Nova edição</h4>
+			
+                <form class="form" id="formulario" action="{{route('editarEdicaoPost', ['edicao'=>$edicao->id])}}" method="post" enctype="multipart/form-data">
+					{{csrf_field()}}
+					<div class="form-group row">
+						<label class="col-sm-2 col-form-label">Data</label>
+						<div class="col-sm-10">
+							<input value="<?php echo $edicao->ed_day."/".$edicao->ed_mounth."/".$edicao->ed_year; ?>" class="form-control" name="data_edicao" type="text" id="calendario">
+						</div>
+					</div>
+					<div id="cadernos">
+						<div class="form-group row">
+							<label class="col-sm-2 col-form-label">Upload do caderno 1 </label>
+							<div class="col-sm-10">
+								<input type="file" class="form-control" name="edicao[]">
+							</div>
+						</div>
+					</div>
 
-        <div class="form-group">
-            {{csrf_field()}}
-        </div>
+					<div class="form-group row">
+						<label class="col-sm-2 col-form-label"></label>
+						<div class="col-sm-10">
+							<button id="novaEdicao" type="button"  class="btn btn-light btn-sm btn-rounded">
+								<i class="fe-plus"></i>
+								Novo carderno
+							</button>
 
-        @if (session()->has('flash.message'))
-        <div class="alert alert-{{ session('flash.class') }}">
-            {{ session('flash.message') }}
-        </div>
-        @endif
+						</div>
+					</div>
 
-        <div class='col-sm-16'>
-
-
-        <div class='col-sm-16'>
-            <div class="form-group">
-                    <label for="basic">Data da Edição</label>
-            </div>
-        </div>
-
-        <div class='col-sm-16'>
-            <div class="form-group">
-                    <p>Data: <input value="<?php echo $edicao->ed_day."/".$edicao->ed_mounth."/".$edicao->ed_year; ?>" name="data_edicao" type="text" id="calendario" /></p>
-                </div>
-            </div>
-
-        <div class='col-sm-16'>
-            <div class="form-group">
-                    <label for="basic">Faça o UPLOAD dos PDFs dos cadernos.</label>
-            </div>
-        </div>
-        <div class='col-sm-16'>
-            <div class="form-group">
-                <input type="button" class="btn" id="novaEdicao" value="Novo caderno" />
-            </div>
-        </div>
-
-        <div class='col-sm-16'>
-            <div id="cadernos">
-                <label>Cardeno 1</label>
-                <input type="file" name="edicao[]">
-            </div>
-        </div>
-
-        <div class='col-sm-16'>
-            <div class="form-group">
-                    <button type="submit" class="btn btn-default">Editar Edição</button>
-            </div>
-        </div>
-
-    </form>
+					<div class="mt-4 mb-1">
+						<div class="text-right d-print-none">
+							<button type="submit" class="btn btn-primary btn-rounded">
+								<i class="fe-upload-cloud"></i>
+								Criar edição
+							</button>
+						</div>
+					</div>
+				</form>
+			</div>
+		</div>
+	</div>
 </div>
-@stop
 
+@stop
