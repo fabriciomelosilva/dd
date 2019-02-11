402980e29109ba5baf28be6ea373cf4b41e0a0be
Fabricio <fabriciomelosilva@gmail.com>
2019-02-11 17:55:46 -0300

    Opção de remover input pdf

--- public/js/custom.js
+++ public/js/custom.js
@@ -16,8 +16,9 @@ jQuery(document).ready(function() {
 		$('#novaEdicao').click(function(e) {
 			e.preventDefault();     //prevenir novos clicks
 			if (x < campos_max) {
-				$('#cadernos').append('<div class="form-group row"><label class="col-sm-2 col-form-label">Upload do caderno '+ x +'</label>\<div class="col-sm-10">\
-						<input  class="form-control" type="file" name="edicao[]">\
+				$('#cadernos').append('<div id="parent" class="form-group row"><label class="col-sm-2 col-form-label">Upload do caderno '+ x +'</label>\<div class="col-sm-10">\
+						<input class="form-control" type="file" name="edicao[]">\
+						<a href="#" class="remover_campo">Remover</a>\
 					<\div>');
 				x++;
 			}
@@ -26,7 +27,7 @@ jQuery(document).ready(function() {
 		// Remover o div anterior
 		$('#cadernos').on("click",".remover_campo",function(e) {
 			e.preventDefault();
-			$(this).parent('div').remove();
+			$(this).parents('#parent').remove();
 			x--;
 		});
 });
