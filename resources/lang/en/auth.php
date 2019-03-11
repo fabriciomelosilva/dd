9864bf7eced901928bfb33c71e8e664d881b4e6f
Fabricio <fabriciomelosilva@gmail.com>
2019-03-11 16:51:23 -0300

    Tradução dos erros de login

--- resources/lang/en/auth.php
+++ resources/lang/en/auth.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'failed' => 'These credentials do not match our records.',
-    'throttle' => 'Too many login attempts. Please try again in :seconds seconds.',
+    'failed' => 'Favor verificar e-mail e senha.',
+    'throttle' => 'Muitas tentativas de login. Por favor tente novamente em :seconds segundos.',
 
 ];
