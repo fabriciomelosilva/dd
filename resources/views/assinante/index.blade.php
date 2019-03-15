e98a50ec4eedb70da193408c708e3346e18a760e
Fabricio <fabriciomelosilva@gmail.com>
2019-03-15 14:57:14 -0300

    Ajustes

--- resources/views/assinante/index.blade.php
+++ resources/views/assinante/index.blade.php
@@ -1,86 +1,96 @@
-@extends('layouts.app')
-@section('content')
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width" />
+    <title>
+      3D FlipBook - Sources forma      </title>
+    <link rel="stylesheet" href="./css/bootstrap.min.css">
+    <link rel="stylesheet" href="./css/bootstrap-theme.min.css">
+    <link rel="stylesheet" href="./css/style.css">
+    <script src="./js/jquery.min.js"></script>
+    <script src="./js/bootstrap.min.js"></script>
+  </head>
 
+  <body>
+    <div class="main-menu">
 
-<script src="{{ URL::to('/js/html2canvas.min.js') }}"></script>
-<script src="{{ URL::to('/js/three.min.js') }}"></script>
-<script src="{{ URL::to('/js/pdf.min.js') }}"></script>
-<script src="{{ URL::to('/js/3dflipbook.min.js') }}"></script>
-
-    
+    </div>
     <div class="container">
-		<div class="content">
-
-
-	<div class="row p-4">
-		<div class="modal fade" id="flip-book-window" tabindex="-1" role="dialog" aria-labelledby="headerLabel">
-			<div class="modal-dialog" role="document">
-				<div class="modal-content">
-				<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span class="glyphicon glyphicon-remove" aria-hidden="true"></span></button>
-				<div class="modal-body">
-					<div class="mount-node">
-					</div>
-				</div>
-				</div>
-			</div>
-		</div>
-
-
-			<div class="books">
-					<h2>PDFs</h2>
-					<div class="thumb">
-						<img id="condoLiving" src="http://localhost:8000/uploadsThumbAssinante/app/edicao/2019/3/12/capa_5c8856e9e6e6c.jpg" class="btn" alt="Condo Living - Party in your place" />
-						<div class="caption">
-						</div>
-					</div>
-			</div>
-
-				<script type="text/javascript">
-
-					var template = {
-					  html: './templates/default-book-view.html',
-					  styles: [
-						'./css/font-awesome.min.css',
-						'./css/short-white-book-view.css'
-					  ],
-					  script: './js/default-book-view.js'
-					};
-		  
-					var booksOptions = {
-						condoLiving: {
-						pdf: "http://localhost:8000/uploadsAssinante/app/edicao/2019/3/24/ed_24_5c88475276fee.pdf",
-						downloadURL: "http://localhost:8000/uploadsAssinante/app/edicao/2019/3/24/ed_24_5c88475276fee.pdf",
-						template: template
-					  },
-		  
-					};
-		  
-					var instance = {
-					  scene: undefined,
-					  options: undefined,
-					  node: $('#flip-book-window').find('.mount-node')
-					};
-		  
-					$('#flip-book-window').on('hidden.bs.modal',  function() {
-					  instance.scene.dispose();
-					});
-					$('#flip-book-window').on('shown.bs.modal', function() {
-					  instance.scene = instance.node.FlipBook(instance.options);
-					});
-		  
-					$('.books').find('img').click(function(e) {
-					  if(e.target.id) {
-						instance.options = booksOptions[e.target.id];
-						$('#flip-book-window').modal('show');
-					  }
-					});
-		  
-				  </script>
-
-		</div>
-
-	
-	</div>
+      <div class="content">
+      
+<div class="modal fade" id="flip-book-window" tabindex="-1" role="dialog" aria-labelledby="headerLabel">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span class="glyphicon glyphicon-remove" aria-hidden="true"></span></button>
+      <div class="modal-body">
+        <div class="mount-node">
+        </div>
+      </div>
+    </div>
+  </div>
 </div>
 
-@endsection
+<div class="books">
+  <h2>PDFs</h2>
+  <div class="thumb">
+    <img id="edicao" src="images/condoLiving.jpg" class="btn" alt="Condo Living - Party in your place" />
+    <div class="caption">
+      PDF magazine as a 3D FlipBook.
+    </div>
+  </div>
+</div>
+
+<script src="./js/html2canvas.min.js"></script>
+<script src="./js/three.min.js"></script>
+<script src="./js/pdf.min.js"></script>
+<script src="./js/3dflipbook.min.js"></script>
+
+<script type="text/javascript">
+  var template = {
+    html: './templates/default-book-view.html',
+    styles: [
+      './css/font-awesome.min.css',
+      './css/short-white-book-view.css'
+    ],
+    script: './js/default-book-view.js'
+  };
+
+  var booksOptions = {
+
+
+    edicao: {
+      pdf: 'http://localhost/flipbook/demo/assets/pdf/magazine.pdf',
+      downloadURL: 'http://localhost/flipbook/demo/assets/pdf/magazine.pdf',
+      template: template
+    },
+
+  };
+
+  var instance = {
+    scene: undefined,
+    options: undefined,
+    node: $('#flip-book-window').find('.mount-node')
+  };
+  $('#flip-book-window').on('hidden.bs.modal',  function() {
+    instance.scene.dispose();
+  });
+  $('#flip-book-window').on('shown.bs.modal', function() {
+    instance.scene = instance.node.FlipBook(instance.options);
+  });
+
+  $('.books').find('img').click(function(e) {
+    if(e.target.id) {
+      instance.options = booksOptions[e.target.id];
+      $('#flip-book-window').modal('show');
+    }
+  });
+
+</script>
+      </div>
+
+    </div>
+
+    
+  </body>
+</html>
---
+++ resources/views/layouts/assinante.blade.php
@@ -0,0 +1,31 @@
+<!doctype html>
+<html lang="pt-BR">
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+		<title>Diário Digital</title>
+
+
+
+
+		<link href="./css/front.css" rel="stylesheet">
+		<link rel="stylesheet" href="http://code.jquery.com/ui/1.9.0/themes/base/jquery-ui.css" />
+		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+		<link href="{{ asset('/css/style.css')}}" rel="stylesheet">
+
+
+		<script src="{{ asset('/js/jquery.min.js')}}"></script>
+		<script src="{{ asset('/js/jquery-ui.js')}}"></script>
+		<script src="{{ asset('/js/bootstrap.min.js')}}"></script>
+
+
+	</head>
+
+	<body class="bg-light">
+		@include('partials.header')
+
+		@yield('content')
+
+		@include('partials.footer')
+	</body>
+</html>
