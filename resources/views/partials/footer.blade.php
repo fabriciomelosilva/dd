68962f9c38b3a942caad625c8af3d92e0d16f9be
Fabricio <fabriciomelosilva@gmail.com>
2019-03-22 08:46:20 -0300

    Ajustes footer e tela de login

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -2,15 +2,9 @@
 	<div class="container-fluid">
 		<div class="row">
 			<div class="col-md-6">
-				Sistema Verdes Mares © 2018
+				Sistema Verdes Mares © 2019
 			</div>
-			<div class="col-md-6">
-				<div class="text-md-right footer-links d-none d-sm-block">
-					<a href="#">About Us</a>
-					<a href="#">Help</a>
-					<a href="#">Contact Us</a>
-				</div>
-			</div>
+	
 		</div>
 	</div>
 </footer>
